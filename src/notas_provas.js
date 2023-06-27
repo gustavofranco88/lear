6ab72d488e3770ba8cad3f6057dbd34b8244539d
gustavofranco88 <gustavofranco88@gmail.com>
2023-06-27 06:54:00 -0300

    finalizando etapa

--- src/notas_provas.js
+++ src/notas_provas.js
@@ -1,6 +1,30 @@
-const express = require(express);
-const app = express();
-const port = 3000;
+const express = require('express');
+const bodyParser = require('body-parser');
+const NotasModel = require('./model.js');
 
-// criar rota padrão
-// criar a cone 
+const app = express();
+app.use(bodyParser.json());
+
+app.post('/notas', async (req, res) => {
+    const { userId, nota, nomeProva } = req.body;
+  
+    try {
+      const novaNota = new NotasModel({
+        userId: userId,
+        nomeProva: nomeProva,
+        nota: nota
+      });
+  
+      await novaNota.save();
+  
+      res.status(201).json({ message: 'Nota salva com sucesso!' });
+    } catch (error) {
+      console.error('Erro ao salvar a nota:', error);
+      res.status(500).json({ message: 'Erro ao salvar a nota.' });
+    }
+  });
+
+  app.listen(3000, () => {
+    console.log('API rodando na porta 3000');
+  });
+  
